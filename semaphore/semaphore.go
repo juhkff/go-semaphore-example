600984f7eb5c4ade399baebeb7a59aa45c0dad39
Liu Yiqing <liuyiqing@xinye.com>
2024-09-23 15:22:50 +0800

    Final Version

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"syscall"
 	"unsafe"
 
@@ -13,47 +14,39 @@ import (
 
 /*
 #include <sys/sem.h>
-#include <sys/types.h> // Add this line to include the necessary header file
-#include <sys/ipc.h> // Add this line to include the necessary header file
 typedef struct sembuf sembuf;
-// 手动定义semun联合体
-union semun {
-    int val;                // Value for SETVAL
-    struct semid_ds *buf;   // Buffer for IPC_STAT, IPC_SET
-    unsigned short *array;  // Array for GETALL, SETALL
-    struct seminfo *__buf;  // Buffer for IPC_INFO (Linux特有)
-};
-
-// 由于semctl是一个variadic函数，需要一个包装函数来正确传递semun参数
-int semctl_setval(int semid, int semnum, int cmd, int val) {
-    union semun arg;
-    arg.val = val;
-    return semctl(semid, semnum, cmd, arg);
-}
 */
 import "C"
 
-var lockFile = "/home/juhkff/projects/go-test/file/lockFile"
-var changeFile = "/home/juhkff/projects/go-test/file/changeFile"
-var configFile = "/home/juhkff/projects/go-test/config/config.yaml"
-
-// 默认信号量key，会被配置文件的值覆盖
+// LockKey 默认信号量key，会被配置文件的值覆盖
 var LockKey = 2216
 
-// 默认并发量，会被配置文件的值覆盖
+// ConcurrentNum 默认并发量，会被配置文件的值覆盖
 var ConcurrentNum = 30
 
+// LockFilePath 默认锁文件路径，会被配置文件的值覆盖
+var LockFilePath = "/usr/local/orchestrator/hook/lockFile"
+
+var configFilePath string
+
 type Config struct {
-	LockKey       int `yaml:"lockKey"`
-	ConcurrentNum int `yaml:"concurrentNum"`
+	LockKey       int    `yaml:"lockKey"`
+	ConcurrentNum int    `yaml:"concurrentNum"`
+	LockFilePath  string `yaml:"lockFilePath"`
 }
 
 func init() {
-	config, err := ReadConfig(configFile)
+	// 配置文件路径在可执行文件同目录下
+	wd, _ := os.Getwd()
+	configFilePath = filepath.Join(wd, "config.yaml")
+	configFile, err := ReadConfig(configFilePath)
 	if err != nil {
 		log.Fatalf("初始化: 读取配置失败: %v\n", err)
 	} else {
-		ConcurrentNum = config.ConcurrentNum
+		LockKey = configFile.LockKey
+		ConcurrentNum = configFile.ConcurrentNum
+		LockFilePath = configFile.LockFilePath
+		LockFilePath = filepath.Join(LockFilePath, "lockFile")
 		log.Printf("读取设定并发量: %d\n", ConcurrentNum)
 	}
 }
@@ -68,42 +61,30 @@ func SetSemaphore() (r1 uintptr, r2 uintptr, err syscall.Errno) {
 	if int(r1) < 0 {
 		return
 	}
-	val := uintptr(C.semctl_setval(C.int(r1), 0, C.SETVAL, C.int(ConcurrentNum)))
-	if int(val) < 0 {
-		//todo :打印errno
-		log.Printf("信号量设值失败\n")
-		r1 = val
+
+	// 准备SETVAL命令的参数
+	cmd := uintptr(C.SETVAL)
+	val := uintptr(ConcurrentNum)
+
+	// 调用SYS_SEMCTL设置信号量值
+	_, _, err = syscall.Syscall6(syscall.SYS_SEMCTL, r1, 0, cmd, val, 0, 0)
+	if err != 0 {
 		return
 	}
 	return
 }
 
-func getLockFile() (file *os.File, err error) {
-	if !fileExists(lockFile) {
-		file, err = os.OpenFile(lockFile, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+func GetLockFile() (file *os.File, err error) {
+	if !fileExists(LockFilePath) {
+		file, err = os.OpenFile(LockFilePath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
 		if err != nil {
 			if os.IsExist(err) {
 				//lock_file已被其它进程创建
-				file, err = os.OpenFile(lockFile, os.O_RDWR|os.O_CREATE, 0666)
+				file, err = os.OpenFile(LockFilePath, os.O_RDWR|os.O_CREATE, 0666)
 			}
 		}
 	} else {
-		file, err = os.OpenFile(lockFile, os.O_RDWR|os.O_CREATE, 0666)
-	}
-	return
-}
-
-func getChangeFile() (file *os.File, err error) {
-	if !fileExists(changeFile) {
-		file, err = os.OpenFile(changeFile, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
-		if err != nil {
-			if os.IsExist(err) {
-				//changeFile已被其它进程创建
-				file, err = os.OpenFile(changeFile, os.O_RDWR|os.O_CREATE, 0666)
-			}
-		}
-	} else {
-		file, err = os.OpenFile(changeFile, os.O_RDWR|os.O_CREATE, 0666)
+		file, err = os.OpenFile(LockFilePath, os.O_RDWR|os.O_CREATE, 0666)
 	}
 	return
 }
@@ -116,7 +97,7 @@ func SemGet() (r1 uintptr, err syscall.Errno, err2 error) {
 	//第一次运行需要初始化信号量
 	if int(r1) < 0 {
 		//创建或获取文件用于锁
-		file, err2 = getLockFile()
+		file, err2 = GetLockFile()
 		if err2 != nil {
 			log.Printf("获取锁文件失败: %v\n", err2)
 			return
@@ -158,11 +139,11 @@ func SemLock(semId int) (r1 uintptr, r2 uintptr, err syscall.Errno) {
 
 	r1, r2, err = syscall.Syscall(syscall.SYS_SEMOP, uintptr(semId), uintptr(unsafe.Pointer(&stSemBuf)), 1)
 	if int(r1) < 0 {
-		log.Printf("请求锁出错: %v\n", err)
+		log.Printf("请求信号量出错: %v\n", err)
 		return
 	}
 	//申请共享锁
-	file, err2 := os.OpenFile(changeFile, os.O_RDWR|os.O_CREATE, 0666)
+	file, err2 := GetLockFile()
 	if err2 != nil {
 		log.Printf("获取共享锁文件失败: %v\n", err2)
 		return
@@ -185,11 +166,11 @@ func SemRelease(semId int) (r1 uintptr, r2 uintptr, err syscall.Errno) {
 
 	r1, r2, err = syscall.Syscall(syscall.SYS_SEMOP, uintptr(semId), uintptr(unsafe.Pointer(&stSemBuf)), 1)
 	if int(r1) < 0 {
-		log.Printf("释放锁出错: %v\n", err)
+		log.Printf("释放信号量出错: %v\n", err)
 		return
 	}
 	//申请共享锁
-	file, err2 := getChangeFile()
+	file, err2 := GetLockFile()
 	if err2 != nil {
 		log.Printf("获取共享锁文件失败: %v\n", err2)
 		return
@@ -217,7 +198,12 @@ func ReadConfig(filePath string) (config Config, err error) {
 		fmt.Printf("无法打开配置文件: %v\n", err)
 		return
 	}
-	defer configFile.Close()
+	defer func(configFile *os.File) {
+		err = configFile.Close()
+		if err != nil {
+			return
+		}
+	}(configFile)
 	configData, err := io.ReadAll(configFile)
 	if err != nil {
 		fmt.Printf("无法读取配置文件: %v\n", err)
